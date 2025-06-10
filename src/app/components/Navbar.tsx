2f81d9a79307193f6b07b61f9ed8b67d2ead363b
BabaVo55 <ethemcode@outlook.com>
2025-06-10 16:06:25 +0100

    all

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Navbar({}: Props) {
+  return (
+    <div className=''>
+        {/* symbols */}
+        <div className=""></div>
+        {/* titles */}
+        <div className=""></div>
+    </div>
+  )
+}
